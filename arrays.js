ec47e4277562a90bfbd7117671489bb209f1527a
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-13 22:14:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToBeginningOfArray(sampleArray, sampleElement) {
 }
 
 function destructivelyAddElementToBeginningOfArray(sampleArray, sampleElement) {
-  sampleArray[10] =sampleElement;
+  sampleArray.unshift(sampleElement);
   return sampleArray
 }
